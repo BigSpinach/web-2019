23113037bd7097079ddec499e77b28ba2065b0fb
BigSpinach <821133937@qq.com>
2019-04-12 02:05:20 +0800

    add H5 and CSS3 files

--- 业务技巧/H5小项目/交互简历/img/readImgList.js
+++ 业务技巧/H5小项目/交互简历/img/readImgList.js
@@ -5,6 +5,6 @@ ary.forEach(function (item) {
     if (/\.(PNG|GIF|JPG)/i.test(item)) {
         //=>图片
         result.push(`img/` + item);
-    }
+    };
 });
 fs.writeFileSync('./result.txt', JSON.stringify(result), 'utf-8');
